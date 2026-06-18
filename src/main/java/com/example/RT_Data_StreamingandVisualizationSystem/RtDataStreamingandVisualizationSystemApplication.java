ce41e8ddd1be5934df9ca0cc8993fd7fff388772
sambhaji_ajane <ajanesambhaji@gmail.com>
2026-06-18 15:09:43 -0400

    Initial commit

---
+++ src/main/java/com/example/RT_Data_StreamingandVisualizationSystem/RtDataStreamingandVisualizationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.RT_Data_StreamingandVisualizationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtDataStreamingandVisualizationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtDataStreamingandVisualizationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RT_Data_StreamingandVisualizationSystem/RtDataStreamingandVisualizationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RT_Data_StreamingandVisualizationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtDataStreamingandVisualizationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
